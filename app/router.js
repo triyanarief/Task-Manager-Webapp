46f8948ad18f9679db1583852455774ceb97b68e
triyanarief <triyanarief@icloud.com>
2017-03-24 20:50:36 +0700

    new tasks

--- app/router.js
+++ app/router.js
@@ -7,7 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('tasks');
+  this.route('tasks', function() {
+    this.route('new');
+  });
 });
 
 export default Router;
